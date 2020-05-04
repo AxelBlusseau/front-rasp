dcd6c09c1757f89e8bed5f166a70f240b3db139a
AxelBlusseau <36446232+AxelBlusseau@users.noreply.github.com>
2020-05-04 11:22:28 +0200

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+        <v-row>
+      <v-col cols="12" md="6">
+        <v-card class="pa-0 ma-0 red">
+          <h1>Allumer en rouge</h1>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="pa-0 ma-0 green">
+          <h1>Allumer en vert</h1>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-card class="pa-0 ma-0 blue">
+          <h1>Allumer en bleu</h1>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="pa-0 ma-0 yellow">
+          <h1>Allumer en jaune</h1>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-card class="pa-0 ma-0 white">
+          <h1>Allumer en blanc</h1>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="pa-0 ma-0 purple">
+          <h1>Allumer en violet</h1>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  
+
+  data: () => ({
+    //
+  }),
+};
+</script>
